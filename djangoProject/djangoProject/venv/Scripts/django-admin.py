0387e2b00daf3daa1004751ca333119a8fd589b3
Keithw96 <c00205321@itcarlow.ie>
2018-02-16 17:19:16 +0000

    First Commit

---
+++ djangoProject/djangoProject/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kelmen\docume~1\colleg~1\web development stuff\final webdev project\djangoproject\djangoproject\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
